0a84c9f24288d1b322ebd4a638161295873a65d4
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2023-10-12 16:26:35 +0300

    add show favourites totals

--- src/components/Favourites/index.tsx
+++ src/components/Favourites/index.tsx
@@ -1,19 +1,32 @@
 import {View, StyleSheet} from 'react-native';
 import {Text, Surface} from 'react-native-paper';
+import {useAppSelector} from '../../hooks/useAppSelector';
 
 function Favourites() {
+  const favourites = useAppSelector(state => state.favourites.favourites);
+
+  const femaleFavsCount = favourites.filter(
+    favourite => favourite.gender === 'female',
+  ).length;
+  const maleFavsCount = favourites.filter(
+    favourite => favourite.gender === 'male',
+  ).length;
+  const otherFavsCount = favourites.filter(
+    favourite => favourite.gender === 'n/a',
+  ).length;
+
   return (
     <View style={styles.favBoxes}>
       <Surface style={styles.favBox}>
-        <Text variant="titleLarge">0</Text>
+        <Text variant="titleLarge">{femaleFavsCount}</Text>
         <Text variant="bodyMedium">Female favs</Text>
       </Surface>
       <Surface style={styles.favBox}>
-        <Text variant="titleLarge">0</Text>
+        <Text variant="titleLarge">{maleFavsCount}</Text>
         <Text variant="bodyMedium">Male favs</Text>
       </Surface>
       <Surface style={styles.favBox}>
-        <Text variant="titleLarge">0</Text>
+        <Text variant="titleLarge">{otherFavsCount}</Text>
         <Text variant="bodyMedium">Others</Text>
       </Surface>
     </View>
